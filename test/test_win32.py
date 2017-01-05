5dfa5fe27422259f7cf4b5e662b402fb2b1e641f
jjones6 <slightlynybbled@gmail.com>
2017-01-05 07:50:36 -0500

    Added custom executable test, renamed to add 'win32' for clarity since the tests are being run on a windows platform

--- test/test_win32.py
+++ test/test_win32.py
@@ -39,16 +39,17 @@ def app_manager(request):
     yield manager
 
     # teardown - delete extra git repository
-    remove_count = 0
+    remove_time = 0.0
+    time_inc = 0.1
     removed = False
-    while remove_count < 5 and not removed:
+    while remove_time < 10.0 and not removed:
         try:
             shutil.rmtree(local_path, onerror=remove_readonly)
             removed = True
         except PermissionError:
-            time.sleep(1.0)
+            time.sleep(time_inc)
 
-        remove_count += 1
+        remove_time += time_inc
 
 
 def test_create_app_manager(app_manager):
@@ -60,6 +61,14 @@ def test_default_exec(app_manager):
 
 
 def test_win32_exec(app_manager):
+    app_manager.load_and_validate()
+    assert app_manager.exec == 'C:/Program Files/Git/bin/git'
+
+
+def test_custom_exec(app_manager):
+    config = app_manager.config
+    config['repository']['executable'] = 'C:/Program Files/Git/bin/git'
+
     app_manager.load_and_validate()
     assert app_manager.exec == 'C:/Program Files/Git/bin/git'
 
