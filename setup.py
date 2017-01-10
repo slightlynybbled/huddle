e645f7f91be4a3564682464cb7dbb646a64ab3d3
Jason R. Jones <slightlynybbled@gmail.com>
2017-01-10 14:12:37 -0500

    Updated setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='huddle',
-    version='0.1.2dev0',
+    version='0.1.2',
     description='A server-oriented auto-deployment utility for any type of program or script',
     author='Jason Jones',
     author_email='slightlynybbled@gmail.com',
